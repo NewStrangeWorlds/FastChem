e30a2058f9b87b6ca1f7cdfd4f96320a1b624ea2
Brett Morris <brettmorris21@gmail.com>
2022-02-08 09:16:31 +0100

    Moving pybind11 header paths

--- python/fastchem_python_wrapper.cpp
+++ python/fastchem_python_wrapper.cpp
@@ -1,5 +1,5 @@
-#include "../_deps/pybind11-src/include/pybind11/pybind11.h"
-#include "../_deps/pybind11-src/include/pybind11/stl.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
 #include "../fastchem_src/fastchem.h"
 #include "../fastchem_src/input_output_struct.h"
 #include "../fastchem_src/fastchem_constants.h"
