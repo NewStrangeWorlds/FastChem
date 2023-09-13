ad039bc1b66ae5e373b60d6d5d7cfab04981b0d7
daniel-kitzmann <daniel.kitzmann@gmail.com>
2023-09-13 16:39:51 +0200

    Add nojekyll

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -16,7 +16,7 @@ release = '3.0'
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ['templates']
 exclude_patterns = []
 
 
@@ -25,7 +25,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ['static']
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
