32d2b57cea5df5d28ac4c927be7d715811abfea5
daniel-kitzmann <daniel.kitzmann@gmail.com>
2025-07-31 10:46:08 +0200

    Change version number in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.errors import CCompilerError
 from distutils import sysconfig
 
 
-__version__ = "3.1.2"
+__version__ = "3.1.3"
 
 def __read__(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
